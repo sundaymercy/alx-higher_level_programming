080d71204199781a9c9432f56f86369682773eb7
sundaymercy <taiwomercy090@gmail.com>
2023-06-15 16:32:38 -0700

    uniq

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def uniq_sum(my_list=[]):
-    sum = 0
+def uniq_add(my_list=[]):
+    add = 0
     for unint in set(my_list):
-        sum += unint
-    return sum
+        add += unint
+    return add
