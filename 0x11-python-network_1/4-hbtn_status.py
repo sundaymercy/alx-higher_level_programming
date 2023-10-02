1f4c6d5cd216930684c04af7612ffe1954479538
sundaymercy <taiwomercy090@gmail.com>
2023-10-02 14:47:13 -0700

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-same as 0-hbtn_status with requests model
-"""
+"""fetches https://intranet.hbtn.io/status."""
+import requests
 
-if __name__ == '__main__':
-    import requests
-    html = requests.get('https://intranet.hbtn.io/status')
+
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(html.text.__class__))
-    print("\t- content: {}".format(html.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
