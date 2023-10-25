7356e7dcc8a3f47f2b6a195a47056ff51668b619
sundaymercy <taiwomercy090@gmail.com>
2023-10-25 16:14:48 -0700

    star

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,21 @@
 #!/usr/bin/node
-// A script that gets the contents of a webpage and stores it in a file.
+// A script that prints the number of movies where the character “Wedge Antilles” is present.
 
-const fs = require('fs');
 const request = require('request');
 
+let count = 0;
 const url = process.argv[2];
-const file = process.argv[3];
 request.get(url, function (error, response, body) {
   if (!error) {
-    fs.writeFileSync(file, body, 'utf-8');
+    const data = JSON.parse(body);
+    const movies = data.results;
+    for (const movie of movies) {
+      for (const character of movie.characters) {
+        if (character.substr(-3) === '18/') {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
   }
 });
